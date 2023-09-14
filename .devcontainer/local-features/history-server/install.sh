a4e183e099f69d4ea03a2d66fc58d5900c766f07
Otacilio Filho <otaciliopedro@gmail.com>
2023-09-14 11:05:19 +0000

    working history-server

--- .devcontainer/local-features/history-server/install.sh
+++ .devcontainer/local-features/history-server/install.sh
@@ -1,1 +1,9 @@
 set -x
+
+cat <<'EOF' >> /usr/local/sdkman/candidates/spark/current/conf/spark-defaults.conf
+spark.eventLog.enabled           true
+spark.eventLog.dir               file:///usr/local/sdkman/candidates/spark/current/spark-events
+spark.history.fs.logDirectory    file:///usr/local/sdkman/candidates/spark/current/spark-events
+EOF
+
+mkdir /usr/local/sdkman/candidates/spark/current/spark-events
