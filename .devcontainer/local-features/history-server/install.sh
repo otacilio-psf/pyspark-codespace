933bb2eaecacdbd05704f26928b5b8c7ef487ee8
Otacilio Filho <otaciliopedro@gmail.com>
2023-09-14 21:07:55 +0000

    update and fix historical server

--- .devcontainer/local-features/history-server/install.sh
+++ .devcontainer/local-features/history-server/install.sh
@@ -7,3 +7,7 @@ spark.history.fs.logDirectory    file:///usr/local/sdkman/candidates/spark/curre
 EOF
 
 mkdir /usr/local/sdkman/candidates/spark/current/spark-events
+
+chmod 777 /usr/local/sdkman/candidates/spark/current/spark-events
+
+chmod +x /usr/local/sdkman/candidates/spark/current/sbin/start-history-server.sh
---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,3 @@
+/bin/bash /usr/local/sdkman/candidates/spark/current/sbin/start-history-server.sh
+
+pip3 --disable-pip-version-check --no-cache-dir install -r requirements.txt
